bd0f5706d0ab0049cd72f5817fecb20c335184ad
Swaraj1701 <samanta.swaraj17@gmail.com>
2025-12-10 12:34:05 +0530

    Add project files.

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await context.Users.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await context.Users.FirstOrDefaultAsync(x=> x.Id == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return member;
+        }
+    }
+}
