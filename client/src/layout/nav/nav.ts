3e836e4753b6e5b40a4e0a4a03819c6dc01fa64f
Swaraj1701 <samanta.swaraj17@gmail.com>
2025-12-11 13:03:41 +0530

    transition, routing,  route guard and toasts

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,28 +1,48 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private router = inject(Router);
+  private toast = inject(ToastService);
   protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: results => {
-        console.log(results);
+      next: () => {
+        this.router.navigateByUrl('/members');
+        this.toast.success('Logged in successfully.')
         this.creds = {};
       },
-      error: error => alert(error.message)
+      error: error => {
+        let message = '';
+        if (error.error?.errors) {
+          const errorsObj = error.error.errors;
+          const allErrors = Object.values(errorsObj).flat();
+          message = allErrors.join('<br>');
+        }
+        else if (typeof error.error === 'string') {
+          message = error.error;
+        }
+        else {
+          message = "Something went wrong";
+        }
+        this.toast.error(message);
+      }
     });
   }
 
   logout() {
     this.accountService.logOut();
+    this.router.navigateByUrl('/');
   }
 }
